627fe92d55edd990d9c07028a384c9238876b7a2
Bigboss <ochie@Bigboss>
2023-11-08 14:24:00 +0300

    my first commit

---
+++ Main.dart/comments.dart
@@ -0,0 +1,11 @@
+//This is the single line comment
+
+/*
+This is a multi-line comment
+This is anotherline in the comment
+*/
+
+// int num = 5;
+
+// String name = "Manuel Shehele";
+
---
+++ one.dart
@@ -0,0 +1,3 @@
+void main() {
+  print("functions practices decision making statements");
+}
