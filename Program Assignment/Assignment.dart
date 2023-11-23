1702120920b7081db68ccb541812d0fb92f96f47
Bigboss <ochie@Bigboss>
2023-11-24 00:55:40 +0300

    assignment

---
+++ Program Assignment/Assignment.dart
@@ -0,0 +1,32 @@
+void main() {
+  //Maps
+  //Maps are used to hold a set of values as keys or value pairs
+  var record = {'Name': 'Emmanuel', 'Age': '23'};
+  record['number'] = "s004";
+  print(record);
+
+// integers
+//Used when whole numbers need to be stored in a variable
+  int myVariable = 39;
+  print(myVariable);
+
+//Doubles
+//used to store double precision(numbers with large decimal points)
+  double Variable = 72.56;
+  print(Variable);
+
+  //Boolean
+  //used to store or represent boolean values 0 and 1
+  var bool = true;
+  print(bool);
+
+//Strings
+//Used to store a series of text
+  String school = "Power Learn Academy";
+  print(school);
+
+  //Lists
+  //is a datatype used to store ordred collection of objects
+  var numbers = [2, 4, 6, 8];
+  print(numbers);
+}
